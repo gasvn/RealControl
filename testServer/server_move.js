7cff6ef0657c5bdcd2d492b3192286c6102f96e6
gasvn <nanoc@live.cn>
2017-05-15 00:28:18 +0800

    new server

---
+++ testServer/server_move.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Conan on 2017/4/22.
+ */
+var CONTROLLER = 0;
+const WebSocket = require('ws');
+var WebSocketServer = require('ws').Server,
+    wss = new WebSocketServer({ port: 3000 });
+    console.log('start');
+wss.on('connection', function (ws) {
+    console.log('client connected');
+    console.log(' has connected to the server');
+    // Broadcast to all.
+
+    ws.on('message', function (message) {
+        console.log(message);
+        var data = JSON.parse(message);
+        if(data.mode===1){
+        	var command = {};
+        	command.action = data.action;
+            wss.broadcast = function broadcast(data) {
+                wss.clients.forEach(function each(client) {
+                    if (client.readyState === WebSocket.OPEN) {
+                        client.send(data);
+                    }
+                });
+            };
+            wss.broadcast(JSON.stringify(command));
+        }
+    });
+});
+
+
